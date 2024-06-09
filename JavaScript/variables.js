05d64271ce6049666da9291a9fc7b6ec5c3afb12
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2024-06-09 14:13:42 -0400

    Added variable and created todo.txt temp file

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,15 +1,11 @@
 let selectedButton = null;
-
 const currentHole = document.getElementById("hole");
 let hole = 1;
-
 const playerNames = [];
-
 const playerDefault = ["Player 1", "Player 2", "Player 3", "Player 4"];
-
+const playerDisplay = document.getElementById("player");
 const prevBtn = document.getElementById("prev-player");
 const nextBtn = document.getElementById("next-player");
 let currentPlayerIndex = 0;
-
 const currentStrokes = document.getElementById("current-strokes");
 let strokes = 0;
