a2e1341589d7b8d169d518ae07d8e7b4607d922c
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2024-06-19 22:04:13 -0400

    Finished Few Features
    - Finished the scoreboard functionality.
    - Modified the next and prev person buttons to add a finish game button on the last hole.

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -70,32 +70,52 @@ function nextHole() {
 
 function prevPlayerHandler() {
   const playerDisplay = document.getElementById("player");
+  const completed = document.getElementById("completed");
+
   currentPlayerIndex =
     currentPlayerIndex === 0
       ? playerDefault.length - 1
       : currentPlayerIndex - 1;
   playerDisplay.innerHTML = playerDefault[currentPlayerIndex];
+
+  if (hole == 18 && playerDisplay.innerText == playerDefault[3]) {
+    completed.innerHTML = "Finish Game";
+    completed.setAttribute("onclick", "finishGame()");
+  }
+
+  if (
+    completed.innerText == "Finish Game" &&
+    playerDisplay.innerText !== playerDefault[3]
+  ) {
+    completed.innerHTML = "Finish Hole";
+    completed.setAttribute("onclick", "finishHole()");
+  }
 }
 
 function nextPlayerHandler() {
   const playerDisplay = document.getElementById("player");
+  const completed = document.getElementById("completed");
+
   currentPlayerIndex = (currentPlayerIndex + 1) % playerDefault.length;
   playerDisplay.innerHTML = playerDefault[currentPlayerIndex];
+
+  if (hole == 18 && playerDisplay.innerText == playerDefault[3]) {
+    completed.innerHTML = "Finish Game";
+    completed.setAttribute("onclick", "finishGame()");
+  }
+
+  if (
+    completed.innerText == "Finish Game" &&
+    playerDisplay.innerText !== playerDefault[3]
+  ) {
+    completed.innerHTML = "Finish Hole";
+    completed.setAttribute("onclick", "finishHole()");
+  }
 }
 
 // end of section //
 
 // Strokes function
-function plusOne() {
-  const currentStrokes = document.getElementById("current-strokes");
-  strokes++;
-  if (strokes <= 0) {
-    strokes = 1;
-  }
-  currentStrokes.innerHTML = "Strokes: " + strokes;
-  return strokes;
-}
-
 function minusOne() {
   const currentStrokes = document.getElementById("current-strokes");
   strokes--;
@@ -106,10 +126,69 @@ function minusOne() {
   return strokes;
 }
 
+function plusOne() {
+  const currentStrokes = document.getElementById("current-strokes");
+  strokes++;
+  if (strokes <= 0) {
+    strokes = 1;
+  }
+  currentStrokes.innerHTML = "Strokes: " + strokes;
+  return strokes;
+}
+
+function player1Total() {
+  const player1Score = document.getElementById("player1");
+
+  for (let i = 0; i <= holeScoreP1.length - 1; i++) {
+    p1Total += holeScoreP1[i];
+    console.log(p1Total);
+  }
+  player1Score.innerHTML = "Player 1: " + p1Total;
+  p1Total = 0;
+  return p1Total;
+}
+
+function player2Total() {
+  const player2Score = document.getElementById("player2");
+
+  for (let i = 0; i <= holeScoreP2.length - 1; i++) {
+    p2Total += holeScoreP2[i];
+    console.log(p2Total);
+  }
+  player2Score.innerHTML = "Player 2: " + p2Total;
+  p2Total = 0;
+  return p2Total;
+}
+
+function player3Total() {
+  const player3Score = document.getElementById("player3");
+
+  for (let i = 0; i <= holeScoreP3.length - 1; i++) {
+    p3Total += holeScoreP3[i];
+    console.log(p3Total);
+  }
+  player3Score.innerHTML = "Player 3: " + p3Total;
+  p3Total = 0;
+  return p3Total;
+}
+
+function player4Total() {
+  const player4Score = document.getElementById("player4");
+
+  for (let i = 0; i <= holeScoreP4.length - 1; i++) {
+    p4Total += holeScoreP4[i];
+    console.log(p4Total);
+  }
+  player4Score.innerHTML = "Player 4: " + p4Total;
+  p4Total = 0;
+  return p4Total;
+}
+
 function finishHole() {
   const playerDisplay = document.getElementById("player");
   const playerNames = playerDisplay.innerText;
   const currentStrokes = document.getElementById("current-strokes");
+  const completed = document.getElementById("completed");
 
   if (playerNames == playerDefault[0]) {
     holeScoreP1[hole - 1] = strokes;
@@ -124,17 +203,26 @@ function finishHole() {
   strokes = 0;
   currentStrokes.innerHTML = "Strokes: " + strokes;
 
-  player1Total();
+  if (playerNames == playerDefault[0]) {
+    player1Total();
+  } else if (playerNames == playerDefault[1]) {
+    player2Total();
+  } else if (playerNames == playerDefault[2]) {
+    player3Total();
+  } else if (playerNames == playerDefault[3]) {
+    player4Total();
+  }
 
-  if (playerNames == playerDefault[3] && hole !== 18) {
+  if (playerNames == playerDefault[3]) {
     nextHole();
-    playerDisplay.innerHTML = playerDefault[0];
-  } else if (hole !== 18) {
+    nextPlayerHandler();
+  } else {
     nextPlayerHandler();
   }
 
-  if (playerNames == playerDefault[3] && hole == 18) {
-    loadPage("scorecard");
+  if (hole == 18 && playerDefault[3]) {
+    completed.innerHTML = "Finish Game";
+    completed.setAttribute("onclick", "finishGame()");
   }
 
   console.log(holeScoreP1);
@@ -143,14 +231,4 @@ function finishHole() {
   console.log(holeScoreP4);
 }
 
-function player1Total() {
-  const player1Score = document.getElementById("player1");
-
-  for (let i = 0; i <= holeScoreP1.length - 1; i++) {
-    p1Total += holeScoreP1[i];
-    console.log(p1Total);
-  }
-  player1Score.innerHTML = "Player 1: " + p1Total;
-  p1Total = 0;
-  return p1Total;
-}
+function finishGame() {}
--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -2,7 +2,7 @@ let selectedButton = null;
 
 let parValue = [];
 
-const heskethCourse = [4, 3, 4, 3, 4, 4, 5, 4, 4, 4, 3, 4, 5, 4, 4, 3, 5, 5]
+const heskethCourse = [4, 3, 4, 3, 4, 4, 5, 4, 4, 4, 3, 4, 5, 4, 4, 3, 5, 5];
 
 const holeScoreP1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 const holeScoreP2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -20,3 +20,6 @@ let currentPlayerIndex = 0;
 let strokes = 0;
 
 let p1Total = 0;
+let p2Total = 0;
+let p3Total = 0;
+let p4Total = 0;
